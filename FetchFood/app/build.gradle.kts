056b2a38fdbb099933d51a0e7e038224f1e68098
Dwayne Dehoedt <dwaynedehoedt.rosch@gmail.com>
2024-11-10 22:09:36 +0530

    updates

--- FetchFood/app/build.gradle.kts
+++ FetchFood/app/build.gradle.kts
@@ -59,4 +59,5 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:33.5.1"))
     implementation("com.google.firebase:firebase-analytics")
     implementation ("com.google.firebase:firebase-auth:21.1.0")
+    implementation(kotlin("script-runtime"))
 }
