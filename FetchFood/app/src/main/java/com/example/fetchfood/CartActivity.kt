3a8fce7bc32d3170f770e948e146b0cea7c10ae7
Dwayne Dehoedt <dwaynedehoedt.rosch@gmail.com>
2024-11-13 11:09:09 +0530

    updated the cart code

--- FetchFood/app/src/main/java/com/example/fetchfood/CartActivity.kt
+++ FetchFood/app/src/main/java/com/example/fetchfood/CartActivity.kt
@@ -3,9 +3,13 @@ package com.example.fetchfood
 import android.os.Bundle
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.fetchfood.models.Product
 
 class CartActivity : AppCompatActivity() {
+
+    private val cartProductList = ArrayList<Product>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +25,21 @@ class CartActivity : AppCompatActivity() {
             finish()
         }
 
-        // Other functionality related to your cart can be added here
+        // Setup RecyclerView
+        viewCart.layoutManager = LinearLayoutManager(this)
+        val adapter = ProductAdapter(cartProductList)
+        viewCart.adapter = adapter
+
+        // Add product from intent to cart
+        val productName = intent.getStringExtra("productName")
+        val productDescription = intent.getStringExtra("productDescription")
+        val productPrice = intent.getIntExtra("productPrice", 0)
+        val productImageURL = intent.getStringExtra("productImage")
+
+        if (productName != null && productDescription != null && productImageURL != null) {
+            val product = Product(productName, productDescription, productPrice, productImageURL)
+            cartProductList.add(product)
+            adapter.notifyDataSetChanged()
+        }
     }
 }
